ef2e737111fa3db4d03a90bacf4304e14f61bc87
Anton Putra <me@antonputra.com>
2021-10-31 21:15:14 +0500

    tmp

--- lessons/089/terraform/subnets.tf
+++ lessons/089/terraform/subnets.tf
@@ -1,8 +1,9 @@
 # Resource: aws_subnet
 # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/subnet
 resource "aws_subnet" "private-us-east-1a" {
-  vpc_id     = aws_vpc.main.id
-  cidr_block = "10.0.0.0/19"
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.0.0/19"
+  availability_zone = "us-east-1a"
 
   tags = {
     "Name"                            = "private-us-east-1a"
@@ -12,8 +13,9 @@ resource "aws_subnet" "private-us-east-1a" {
 }
 
 resource "aws_subnet" "private-us-east-1b" {
-  vpc_id     = aws_vpc.main.id
-  cidr_block = "10.0.32.0/19"
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.32.0/19"
+  availability_zone = "us-east-1b"
 
   tags = {
     "Name"                            = "private-us-east-1b"
@@ -23,8 +25,9 @@ resource "aws_subnet" "private-us-east-1b" {
 }
 
 resource "aws_subnet" "public-us-east-1a" {
-  vpc_id     = aws_vpc.main.id
-  cidr_block = "10.0.64.0/19"
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.64.0/19"
+  availability_zone = "us-east-1a"
 
   tags = {
     "Name"                       = "public-us-east-1a"
@@ -34,8 +37,9 @@ resource "aws_subnet" "public-us-east-1a" {
 }
 
 resource "aws_subnet" "public-us-east-1b" {
-  vpc_id     = aws_vpc.main.id
-  cidr_block = "10.0.96.0/19"
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.96.0/19"
+  availability_zone = "us-east-1b"
 
   tags = {
     "Name"                       = "public-us-east-1b"
